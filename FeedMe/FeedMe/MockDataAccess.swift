71d21dcd1f488e1a420426681589c9d80b498986
Roland Huhn <roland.huhn@3pillarglobal.com>
2016-10-12 12:56:17 +0300

    changed initialization

--- FeedMe/FeedMe/MockDataAccess.swift
+++ FeedMe/FeedMe/MockDataAccess.swift
@@ -11,23 +11,19 @@ import Foundation
 class MockDataAccess: DataAccess {
     func getMenuFor(_ date: Date, completionHandler: ([Product], NSError?) -> Void) {
         
-        let product1 = Product(name: "Goulash", price: 20, imageUrl: Bundle.main.path(forResource: "Goulash", ofType: "jpg"))
-        /*
-        product1.name = "Goulash"
-        product1.price = 20
-        product1.imageUrl = Bundle.main.path(forResource: "Goulash", ofType: "jpg")*/
+        let product1 = Product(name: "Goulash", price: 20, imageUrl: Bundle.main.path(forResource: "Goulash", ofType: "jpg")!)
         
-        let product2 = Product()
+        var product2 = Product()
         product2.name = "Pizza"
         product2.price = 25
         product2.imageUrl = Bundle.main.path(forResource: "pizza", ofType: "jpg")
         
-        let product3 = Product()
+        var product3 = Product()
         product3.name = "Shawarma"
         product3.price = 15
         product3.imageUrl = Bundle.main.path(forResource: "Shawarma", ofType: "jpg")
         
-        let productList : [Product] = [ product1, product2, product3 ]
+        let productList : [Product] = [product1, product2, product3 ]
         
         completionHandler(productList, nil)
     }
